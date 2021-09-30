19322e305d0a75d37544835f858379df24390bcc
Gildeir <gildeirlopes@gmail.com>
2021-09-30 18:40:13 -0300

    corrigindo typo exec_mode

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const PORT = process.env.PORT || 3000;
+const { PORT } = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
